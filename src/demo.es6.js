090d5ae09113a23dde311719ea9c07146b59f883
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-13 15:16:13 +0200

    create demo usage files

---
+++ src/demo.es6.js
@@ -0,0 +1,5 @@
+import CustomForm from './customForm.js';
+import CustomFormControl from './customFormControl.js';
+
+window.customElements.define('custom-form', CustomForm);
+window.customElements.define('custom-formcontrol', CustomFormControl);
---
+++ src/demo.js
@@ -0,0 +1,7 @@
+import 'document-register-element';
+import 'dom4';
+import '../lib/validityState-polyfill.js';
+import 'core-js/es6/map';
+import 'core-js/es6/symbol';
+
+import './demo.es6.js';
