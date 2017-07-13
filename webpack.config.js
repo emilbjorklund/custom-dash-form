c7baa5eb17ed4fa6bd61fca983030f810e8959de
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-13 15:16:38 +0200

    add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const pkg = require('./package');
+const webpack = require('webpack');
+const path = require('path');
+
+const uglify_options = {
+  compress: {
+    drop_debugger: true,
+    drop_console: true
+  },
+  output: {
+    comments: false
+  }
+}
+
+module.exports = {
+  entry: {
+    demo: './src/demo.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env']]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin(uglify_options),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  output: {
+    filename: './dist/[name].js'
+  }
+}
