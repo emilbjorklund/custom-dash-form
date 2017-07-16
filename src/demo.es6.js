fdc3dadecba9ff697873436227cd8b0b40a718e4
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-16 20:03:08 +0200

    update demo js to include ‘.js’

--- src/demo.es6.js
+++ src/demo.es6.js
@@ -1,5 +1,5 @@
-import CustomForm from './elements/CustomForm';
-import CustomFormControl from './elements/CustomFormControl';
+import CustomForm from './elements/CustomForm/index.js';
+import CustomFormControl from './elements/CustomFormControl/index.js';
 
 window.customElements.define('custom-form', CustomForm);
 window.customElements.define('custom-formcontrol', CustomFormControl);
