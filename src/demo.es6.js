3ed865f277f588569969852a2bd10a00569b551c
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-16 18:45:45 +0200

    update demo ES6 file

--- src/demo.es6.js
+++ src/demo.es6.js
@@ -1,5 +1,5 @@
-import CustomForm from './customForm.js';
-import CustomFormControl from './customFormControl.js';
+import CustomForm from './elements/CustomForm';
+import CustomFormControl from './elements/CustomFormControl';
 
 window.customElements.define('custom-form', CustomForm);
 window.customElements.define('custom-formcontrol', CustomFormControl);
