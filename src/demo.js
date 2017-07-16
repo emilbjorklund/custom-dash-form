12cbeb665ead5243f68d64d672ce73312a473492
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-16 18:45:58 +0200

    update polyfills in demo file

--- src/demo.js
+++ src/demo.js
@@ -1,7 +1,8 @@
+import 'core-js/es6/map';
+import 'core-js/es6/promise';
+import 'core-js/es6/array';
 import 'document-register-element';
 import 'dom4';
 import '../lib/validityState-polyfill.js';
-import 'core-js/es6/map';
-import 'core-js/es6/symbol';
 
 import './demo.es6.js';
