8f583080203fbdc55c7e68aa40e916d620804688
Emil Björklund <emil.bjorklund@inuse.se>
2017-07-21 11:09:12 +0200

    stop debugging webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 browsers: pkg.browserlist
               },
               useBuiltins: true,
-              debug: true
+              //debug: true
             }]]
           }
         }
